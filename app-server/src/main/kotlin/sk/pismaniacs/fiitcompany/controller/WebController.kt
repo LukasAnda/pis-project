a7955ef86a915485ac78e40a6a7bc39580eba6ab
Lukas Anda <lukas.anda@gmail.com>
2020-04-25 16:48:11 +0200

    Add items to WebController

--- app-server/src/main/kotlin/sk/pismaniacs/fiitcompany/controller/WebController.kt
+++ app-server/src/main/kotlin/sk/pismaniacs/fiitcompany/controller/WebController.kt
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import java.util.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import sk.pismaniacs.fiitcompany.repository.ItemRepository
+import sk.pismaniacs.fiitcompany.repository.NotificationRepository
+import sk.pismaniacs.fiitcompany.repository.OrderRepository
+import sk.pismaniacs.fiitcompany.repository.SeasonRepository
 
 
 @Controller
@@ -15,11 +19,31 @@ class WebController {
     fun WebAppContoller(environment: Environment) {
         appMode = environment.getProperty("app-mode") ?: ""
     }
+
+    @Autowired
+    lateinit var itemRepository: ItemRepository
+
+    @Autowired
+    lateinit var orderRepository: OrderRepository
+
+    @Autowired
+    lateinit var notificationRepository: NotificationRepository
+
+    @Autowired
+    lateinit var seasonRepository: SeasonRepository
+
+
     @RequestMapping("/")
     fun index(model: Model): String {
         model.addAttribute("datetime", Date())
         model.addAttribute("username", "Peder")
         model.addAttribute("mode", appMode)
         return "index"
+    }
+
+    @RequestMapping("/allProducts")
+    fun getAllProducts(model: Model): String{
+        model.addAttribute("items", itemRepository.findAll().sortedBy { it.id })
+        return "TuSiDajNazovStranky"
     }
 }
