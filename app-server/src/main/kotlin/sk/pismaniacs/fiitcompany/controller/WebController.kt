0ed6caedd02b586e0b065f2d3916850cef17b545
Peter Hlavatík <32091206+PeHk@users.noreply.github.com>
2020-05-02 11:00:47 +0200

    New screen added

---
+++ app-server/src/main/kotlin/sk/pismaniacs/fiitcompany/controller/WebController.kt
@@ -0,0 +1,48 @@
+package sk.pismaniacs.fiitcompany.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import sk.pismaniacs.fiitcompany.repository.ItemRepository
+import sk.pismaniacs.fiitcompany.repository.NotificationRepository
+import sk.pismaniacs.fiitcompany.repository.OrderRepository
+import sk.pismaniacs.fiitcompany.repository.SeasonRepository
+
+
+@Controller
+class WebController {
+    lateinit var appMode: String
+    @Autowired
+    fun WebAppContoller(environment: Environment) {
+        appMode = environment.getProperty("app-mode") ?: ""
+    }
+
+    @Autowired
+    lateinit var itemRepository: ItemRepository
+
+    @Autowired
+    lateinit var orderRepository: OrderRepository
+
+    @Autowired
+    lateinit var notificationRepository: NotificationRepository
+
+    @Autowired
+    lateinit var seasonRepository: SeasonRepository
+
+
+    @RequestMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("mode", appMode)
+        model.addAttribute("items", itemRepository.findAll().sortedBy { it.id })
+        return "index"
+    }
+
+    @RequestMapping("/allProducts")
+    fun getAllProducts(model: Model): String{
+        model.addAttribute("items", itemRepository.findAll().sortedBy { it.id })
+        return "edit"
+    }
+}
